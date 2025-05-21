724c28cf8b986fd84e1017ab70ccae32cdcadd91
WillJP23 <WILL.PEARSALL23@bathspa.ac.uk>
2025-05-21 23:00:41 +0100

    Added player input & square selection

--- Project Files/Tic_Tac_Code/Tic_Tac_Code.ino
+++ Project Files/Tic_Tac_Code/Tic_Tac_Code.ino
@@ -20,18 +20,101 @@ void setup()
   pinMode(4, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(2, OUTPUT);
+  //GreenRed = false, means that the current player is red
+  bool Pos1 = false, Pos2 = false, Pos3 = false, Pos4 = false, Pos5 = false, Pos6 = false, Pos7 = false, Pos8 = false, Pos9 = false, GreenRed = false;
+  string Pos;
 }
 
 void loop()
 {
+  //Outputs Current Player to Console
+  if (GreenRed = false) {
+    cout << "Current Player: Red\n";
+    cout << "Please Select a Valid Square\n":
+    //add a grid here?
+    cin >> Pos;
+    //determines which square is chosen
+    if (Pos = '1') {
+      Pos1 = true;
+    }
+    else if (Pos = '2') {
+      Pos2 = true;
+    }
+    else if (Pos = '3') {
+      Pos2 = true;
+    }
+    else if (Pos = '4') {
+      Pos2 = true;
+    }
+    else if (Pos = '5') {
+      Pos2 = true;
+    }
+    else if (Pos = '6') {
+      Pos2 = true;
+    }
+    else if (Pos = '7') {
+      Pos2 = true;
+    }
+    else if (Pos = '8') {
+      Pos2 = true;
+    }
+    else if (Pos = '9') {
+      Pos2 = true;
+    }    
+  }
+  else if (GreenRed = true) {
+    cout << "Current Player: Green\n";
+    cout << "Please Select a Valid Square\n":
+    cin >> Pos;  
+
+    if (Pos = '1') {
+      Pos1 = true;
+    }
+    else if (Pos = '2') {
+      Pos2 = true;
+    }
+    else if (Pos = '3') {
+      Pos2 = true;
+    }
+    else if (Pos = '4') {
+      Pos2 = true;
+    }
+    else if (Pos = '5') {
+      Pos2 = true;
+    }
+    else if (Pos = '6') {
+      Pos2 = true;
+    }
+    else if (Pos = '7') {
+      Pos2 = true;
+    }
+    else if (Pos = '8') {
+      Pos2 = true;
+    }
+    else if (Pos = '9') {
+      Pos2 = true;
+    }  
+  }
+
+  //if current player is green/red and a posistion 1 is selected, then 
+  if (Pos1 = true && GreenRed = false) {
+    digitalWrite(13, HIGH);
+    GreenRed = true;
+    Pos1 = false;
+  }
+  else if (Pos1 = true && GreenRed = true) {
+     digitalWrite(12, HIGH);
+    GreenRed = false;
+    Pos1 = false;
+  }
+
+  //need to work out the rest
   digitalWrite(A5, HIGH);
   digitalWrite(A4, HIGH);
   digitalWrite(A3, HIGH);
   digitalWrite(A2, HIGH);
   digitalWrite(A1, HIGH);
   digitalWrite(A0, HIGH);
-  digitalWrite(13, HIGH);
-  digitalWrite(12, HIGH);
   digitalWrite(11, HIGH);
   digitalWrite(10, HIGH);
   digitalWrite(9, HIGH);
