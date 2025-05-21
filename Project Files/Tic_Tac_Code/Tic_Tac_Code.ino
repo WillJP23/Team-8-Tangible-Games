ebb386404b18ff350be0f7d4666928485c431c53
WillJP23 <WILL.PEARSALL23@bathspa.ac.uk>
2025-05-22 00:16:30 +0100

    Win conditions
    added red and green win conditions for a 3-in-a-row

--- Project Files/Tic_Tac_Code/Tic_Tac_Code.ino
+++ Project Files/Tic_Tac_Code/Tic_Tac_Code.ino
@@ -23,13 +23,66 @@ void setup()
   //greenred = false, means that the current player is red
   bool pos1 = false, pos2 = false, pos3 = false, pos4 = false, pos5 = false, pos6 = false, pos7 = false, pos8 = false, pos9 = false, greenred = false;
   //variables for tracking greenred posisitions, used for calculating winner
-  bool r1 = false, r2 = false, r3 = false, r4 = false, r5 = false, r6 = false, r7 = false, r8 = false, r9 = false;
-  bool g1 = false, g2 = false, g3 = false, g4 = false, g5 = false, g6 = false, g7 = false, g8 = false, g9 = false;
+  bool r1 = false, r2 = false, r3 = false, r4 = false, r5 = false, r6 = false, r7 = false, r8 = false, r9 = false, redwin = false;
+  bool g1 = false, g2 = false, g3 = false, g4 = false, g5 = false, g6 = false, g7 = false, g8 = false, g9 = false, greenwin = false;
   string pos;
 }
 
 void loop()
 {
+  //sequence determining if a winner is here
+  //Green Win Conditions
+  // - Horizontals
+  if (g1 = true && g2 = true && g3 = true )
+    greenwin = true;
+  if (g4 = true && g5 = true && g6 = true )
+    greenwin = true;
+  if (g7 = true && g8 = true && g9 = true )
+    greenwin = true;
+  // - Verticals
+  if (g1 = true && g4 = true && g7 = true )
+    greenwin = true;
+  if (g2 = true && g5 = true && g8 = true )
+    greenwin = true;
+  if (g3 = true && g6 = true && g9 = true )
+    greenwin = true;
+  // - Diagonals
+  if (g1 = true && g5 = true && g9 = true )
+    greenwin = true;
+  if (g3 = true && g5 = true && g7 = true )
+    greenwin = true;
+  //Red Win Conditions
+    if (r1 = true && r2 = true && r3 = true )
+    redwin = true;
+  if (r4 = true && r5 = true && r6 = true )
+    redwin = true;
+  if (r7 = true && r8 = true && r9 = true )
+    redwin = true;
+  if (r1 = true && r4 = true && r7 = true )
+    redwin = true;
+  if (r2 = true && r5 = true && r8 = true )
+    redwin = true;
+  if (r3 = true && r6 = true && r9 = true )
+    redwin = true;
+  if (r1 = true && r5 = true && r9 = true )
+    redwin = true;
+  if (r3 = true && r5 = true && r7 = true )
+    redwin = true;
+
+//---------------------------------------------------------------------------------------------------
+  //winner script
+  if (greenwin = true) {
+    cout << "Winner: Green Player\n";
+    //need to add other positions after
+    digitalWrite(12, HIGH);
+  }
+  if (redwin = true) {
+    cout << "Winner: Red Player\n";
+    //need to add other positions after
+    digitalWrite(13, HIGH);
+  }
+
+//---------------------------------------------------------------------------------------------------
   //Outputs Current Player to Console
   if (greenred = false) {
     cout << "Current Player: Red\n";
@@ -43,9 +96,10 @@ void loop()
   //add a grid here?
   cin >> pos;
   //determines which square is chosen
+  //can add code below this section into this part to make more effecient
   if (pos = '1') {
-    if 
     pos1 = true;
+    //here for example: if / else if
   }
   else if (Pos = '2') {
     pos2 = true;
@@ -70,7 +124,11 @@ void loop()
   }
   else if (Pos = '9') {
     pos9 = true;
-  }    
+  }
+  else {
+    //make sure this works
+    cout << "Please Input a Valid Posistion\n";
+  }
 
 //---------------------------------------------------------------------------------------------------
   //if current player is green/red and a posistion 1 is selected, then 
