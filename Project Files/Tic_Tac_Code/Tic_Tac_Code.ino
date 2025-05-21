cc5bdf91a3026a42db027a05716bed1507de418a
WillJP23 <WILL.PEARSALL23@bathspa.ac.uk>
2025-05-21 22:26:02 +0100

    Added Beginning Code
    Basic Code imported from Tinkercad

---
+++ Project Files/Tic_Tac_Code/Tic_Tac_Code.ino
@@ -0,0 +1,46 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(A5, OUTPUT);
+  pinMode(A4, OUTPUT);
+  pinMode(A3, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(A0, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(2, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(A5, HIGH);
+  digitalWrite(A4, HIGH);
+  digitalWrite(A3, HIGH);
+  digitalWrite(A2, HIGH);
+  digitalWrite(A1, HIGH);
+  digitalWrite(A0, HIGH);
+  digitalWrite(13, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(2, HIGH);
+  delay(10); // Delay a little bit to improve simulation performance
+}
